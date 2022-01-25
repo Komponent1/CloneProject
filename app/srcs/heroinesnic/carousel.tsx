3ceee21902480b88c03ea6d6ea8120eae5ff41a7
seo2im <seo2im6492@gmail.com>
2022-01-25 20:29:37 +0900

    feat: update section5 btncarousel

---
+++ app/srcs/heroinesnic/carousel.tsx
@@ -0,0 +1,39 @@
+import React, { Children, useState, useRef, useEffect } from 'react';
+
+const BtnCarousel: React.FC = ({ children }) => {
+  const [ idx, setIdx ] = useState<number>(0);
+  const refs = useRef<React.Ref[]>(
+    Array.from({  length: children.length }).map(() => React.createRef())
+  );
+  const [ isChanged, setIsChanged ] = useState<boolean>(false);
+
+  const click = (i: number) => {
+    console.log(refs);
+    if (idx === i || isChanged) return;
+    refs.current[idx].current.style.setProperty('--trans', 0);
+    setIsChanged(true);
+    setIdx(i);
+  }
+  useEffect(() => {
+    refs.current[idx].current.style.setProperty('--trans', 1);
+  }, [ idx ])
+
+  return (
+    <div className="carousel">
+      <div className="wrapper">
+        {children.map((elem, i) => React.cloneElement(elem, {
+          onTransitionEnd: () => setIsChanged(false),
+          ref: refs.current[i],
+          className: 'item'
+        }))}
+      </div>
+      <div className="btn">
+        {children.map((_, i) => (
+          <button className={i === idx ? 'select' : 'none'} onClick={() => click(i)} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BtnCarousel;
--- app/srcs/heroinesnic/section_five.tsx
+++ app/srcs/heroinesnic/section_five.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import BtnCarousel from './carousel';
 
 const SectionFive: React.FC = () => (
   <section id="section5">
-    <div className="carrosel">
-
+    <div className="left">
+      <BtnCarousel>
+        <div style={{ width: '100%', height: '100%', background: 'yellow'}} />
+        <div style={{ width: '100%', height: '100%', background: 'red'}} />
+        <div style={{ width: '100%', height: '100%', background: 'green'}} />
+        <div style={{ width: '100%', height: '100%', background: 'grey'}} />
+      </BtnCarousel>
     </div>
-    <div className="img">
+    <div className="right">
 
     </div>
   </section>
